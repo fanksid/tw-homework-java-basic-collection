be362b371e4816e6fc32f0a586e406f4fa89477a
superxc <superxcgm@gmail.com>
2018-04-04 19:17:18 +0800

    finish getSumOfEvens and getSumofOdds

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -2,15 +2,54 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 public class Add {
+    public static void main(String[] args) {
+        
+    }
     public int getSumOfEvens(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        return getSumOfOddsOrEvens(leftBorder, rightBorder, true);
     }
 
     public int getSumOfOdds(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        return getSumOfOddsOrEvens(leftBorder, rightBorder, false);
+
+    }
+
+    private int getSumOfOddsOrEvens(int leftBorder, int rightBorder, boolean isGetEvenSum) {
+        // make sure that leftBorder <= rightBorder, do this for convenient
+        Integer firstI = leftBorder;
+        Integer lastI = rightBorder;
+        swapIfFirstGreaterThanSecond(firstI, lastI);
+        int mod = isGetEvenSum ? 1 : 0;
+        if (firstI % 2 == mod) {
+            firstI++;
+        }
+        if (lastI % 2 == mod) {
+            lastI--;
+        }
+        int nEle = (lastI - firstI) / 2 + 1;
+        // 等差数列求和
+        return (firstI + lastI) * nEle / 2;
+    }
+
+    private void swapIfFirstGreaterThanSecond(Integer fistI, Integer secondI) {
+        if (fistI == null || secondI == null || fistI <= secondI) {
+            return;
+        }
+
+        Class intCl = fistI.getClass();
+        try {
+            Field value = intCl.getDeclaredField("value");
+            value.setAccessible(true);
+            int tmp = fistI;
+            value.setInt(fistI, secondI);
+            value.setInt(secondI, tmp);
+        } catch (NoSuchFieldException | IllegalAccessException  e) {
+            e.printStackTrace();
+        }
     }
 
     public int getSumTripleAndAddTwo(List<Integer> arrayList) {
