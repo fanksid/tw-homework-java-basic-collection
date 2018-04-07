43d0e342f1d328c25fd633d2801fccc221604ae0
superxc <superxcgm@gmail.com>
2018-04-07 15:55:57 +0800

    finish almost Reduce but getMedianInLinkList

--- src/main/java/com/thoughtworks/collection/Reduce.java
+++ src/main/java/com/thoughtworks/collection/Reduce.java
@@ -1,8 +1,10 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import javafx.util.Pair;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.ListIterator;
 
 public class Reduce {
 
@@ -13,43 +15,87 @@ public class Reduce {
     }
 
     public int getMaximum() {
-        throw new NotImplementedException();
+        return Collections.max(arrayList);
     }
 
     public double getMinimum() {
-        throw new NotImplementedException();
+        return Collections.min(arrayList);
     }
 
     public double getAverage() {
-        throw new NotImplementedException();
+        int sum = 0;
+        for (Integer elemI : arrayList) {
+            sum += elemI;
+        }
+        return 1.0 * sum / arrayList.size();
     }
 
     public double getOrderedMedian() {
-        throw new NotImplementedException();
-    }
-
-    public int getFirstEven() {
-        throw new NotImplementedException();
-    }
-
-    public int getIndexOfFirstEven() {
-        throw new NotImplementedException();
-    }
-
-    public boolean isEqual(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        int size = arrayList.size();
+        int lastIndex = size - 1;
+        if (size % 2 == 1) {
+            return arrayList.get(lastIndex / 2);
+        } else {
+            return (arrayList.get(lastIndex / 2) + arrayList.get(lastIndex / 2 + 1)) / 2.0;
+        }
     }
 
     //实现接口SingleLink，然后再此函数内使用
     public Double getMedianInLinkList(SingleLink singleLink) {
-        throw new NotImplementedException();
+
+    }
+
+    public int getFirstEven() {
+        for (Integer elemI : arrayList) {
+            if (elemI % 2 == 0) {
+                return elemI;
+            }
+        }
+        return -1;
+    }
+
+    public int getIndexOfFirstEven() {
+        // 这里不用for(int i = 0; i < arrayList.size(); i++)
+        // 的原因是，arrayList可能使用的是链表实现，链表不支持随机访问，所以链表的get()开销比较大
+        int index = 0;
+        for (Integer elemI : arrayList) {
+            if (elemI % 2 == 0) {
+                return index;
+            }
+            index++;
+        }
+        return -1;
     }
 
     public int getLastOdd() {
-        throw new NotImplementedException();
+        Integer rtnI = findLastOdd().getKey();
+        // 返回偶数表示不存在奇数
+        return rtnI == null ? 0 : rtnI;
     }
 
     public int getIndexOfLastOdd() {
-        throw new NotImplementedException();
+        Integer rtnI =findLastOdd().getValue();
+        return rtnI == null ? -1 : rtnI;
+    }
+
+    /**
+     * find last odd in the list
+     * @return a Pair, Key means the odd, and the Value means the index
+     */
+    private Pair<Integer, Integer> findLastOdd() {
+        int index = arrayList.size() - 1;
+        ListIterator<Integer> it = arrayList.listIterator(arrayList.size());
+        while (it.hasPrevious()) {
+            int elemI = it.previous();
+            if (elemI % 2 == 1) {
+                return new Pair<>(elemI, index);
+            }
+            index--;
+        }
+        return new Pair<>(null, null);
+    }
+
+    public boolean isEqual(List<Integer> arrayList) {
+        return this.arrayList.equals(arrayList);
     }
 }
