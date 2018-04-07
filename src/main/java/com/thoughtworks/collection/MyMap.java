e1b065f67df98fc05d67d689175c88d8ee7b345f
superxc <superxcgm@gmail.com>
2018-04-07 15:05:09 +0800

    finish MyMap

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -2,7 +2,9 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class MyMap {
@@ -17,22 +19,46 @@ public class MyMap {
     }
 
     public List<Integer> getTriple() {
-        throw new NotImplementedException();
+        List<Integer> listTriple = new ArrayList<>();
+        for (Integer elemI : array) {
+            listTriple.add(elemI * 3);
+        }
+        return listTriple;
     }
 
     public List<String> mapLetter() {
-        throw new NotImplementedException();
+        List<String> listLetter = new ArrayList<>();
+        for (Integer elemI : array) {
+            listLetter.add("" + (char) ('a' - 1 + elemI));
+        }
+        return listLetter;
     }
 
     public List<String> mapLetters() {
-        throw new NotImplementedException();
+        List<String> listLetters = new ArrayList<>();
+        for (Integer elemI : array) {
+            listLetters.add(int2Letters(elemI));
+        }
+        return listLetters;
+    }
+
+    private String int2Letters(int x) {
+        // 这个代码仅能处理本题的测试数据
+        int quotient;
+        x--;
+        String sFirst = (quotient = x / 26) > 0 ? letters[quotient - 1] : "";
+        return sFirst + letters[x % 26];
     }
 
     public List<Integer> sortFromBig() {
-        throw new NotImplementedException();
+        List<Integer> listNonDecrease = new ArrayList<>(array);
+        listNonDecrease.sort(Comparator.reverseOrder());
+        return listNonDecrease;
     }
 
     public List<Integer> sortFromSmall() {
-        throw new NotImplementedException();
+        List<Integer> listNonDecrease = new ArrayList<>(array);
+        listNonDecrease.sort(null);
+        return listNonDecrease;
     }
 }
